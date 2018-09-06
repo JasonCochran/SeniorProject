69d71e03f9cda4f10bdda27d07a90b81cdace1db
Greg Fussell <charlesfussell@ufl.edu>
2018-09-06 18:30:56 -0400

    added a test program

--- precog/app/precog.py
+++ precog/app/precog.py
@@ -1,4 +1,9 @@
 from app import db
 from app.models import Prediction, Incident
-
+from flask_sqlalchemy import SQLAlchemy
 # Write pre cog here
+def getIncidents(numRows):
+    data = db.session.query(Incident).limit(5)
+    print(data[0].arrest)
+getIncidents(10)
+    
