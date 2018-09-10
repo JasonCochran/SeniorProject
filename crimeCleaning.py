9339164592b430a550b9b6d11ae984266f2b71c6
Jason Cochran <j.a.cochran.cs@gmail.com>
2018-09-09 21:49:49 -0400

    Adding basic GUI components

--- crimeCleaning.py
+++ crimeCleaning.py
@@ -4,7 +4,12 @@ import pandas as pd
 
 df = pd.read_csv('crimeCSV/Crimes_-_2001_to_present.csv')
 
+print("Rows before: ") 
 print(df.shape)
+
 df = df.dropna()
+
+print("Rows after removing ALL NA's: ")
 print(df.shape)
+
 df.to_csv('crimeCSV/crimeCleaned.csv', index=False)
