24b7699ac23850b9bfac357971e8338a4c383868
Jason Cochran <j.a.cochran.cs@gmail.com>
2018-09-12 15:26:27 -0400

    Basics for stats precog

--- stat_precog/app/precog.py
+++ stat_precog/app/precog.py
@@ -8,15 +8,29 @@ def getIncidents(numRows):
     print(data[0].arrest)
 
 def runStats(numRows):
-	for blah:
-		box = db.session.query(Incident).filter()
-		
-		# Collect all data within our box
-		# Record how many crimes occur in each box
-	for blah:
-		# Compare our boxes crime to all other boxes
-		# Compute stats and record them as actual prediction
-	
-getIncidents(10)
-runStats(
+	originLat = os.environ['LATORG']
+	originLong = os.environ['LONORG']
+	limitLat = os.environ['LATSTOP']
+	limitLong = os.environ['LONSTOP']
 
+	locations = 
+
+	for x in range(originLat, limitLat): # X loop
+		for y in range(originLong, limitLong): # Y loop
+			# Create a selection box to get all crimes in a certain region
+			box = "POLYGON((" + p1x + " " + p1y + ", " + p2x + " " + p2y + ", " + p3x + " " + p3y + ", "     /
+				+ p4x + " " + p4y + "))"
+			print(box)
+			result = db.session.query(Incident).filter(Incident.geom.contains(box)).count()
+
+			# Record how many crimes occur in each box
+			
+
+	# Run stats over the array
+
+	for x in range(originLat, limitLat): # X loop
+		for y in range(originLong, limitLong): # Y loop
+			# Compare our boxes crime to all other boxes
+			# Compute stats and record them as actual prediction
+
+runStats(10)
