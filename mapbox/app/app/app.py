5506b8190a96ebf2eaf7d4621abc3dcd8e298bad
Jason Cochran <j.a.cochran.cs@gmail.com>
2018-09-24 01:03:12 -0400

    More work on GUI. JSON serialization not working though

--- mapbox/app/app/app.py
+++ mapbox/app/app/app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
 from werkzeug import secure_filename
 from app import app, db, models
-import os, sys
+import os, sys, requests
 
 @app.route('/',methods=['GET','POST'])
 def my_maps():
+	# Can we request from dblayer:81 ?? It should be in the same Docker network
+	response = requests.get('172.18.0.7:81/jsonData/test.json')
+	#print(response.json)
+	return render_template('index.html')
 
- #  mapbox_access_token = 'pk.eyJ1IjoibWV0cmljb24iLCJhIjoiY2l3eTQxMWl3MDBmYTJ6cWg3YmZtdjdsMSJ9.2vDbTw3ysscpy3YWkHo6aA'
-  mapbox_access_token = ''
-  return render_template('index.html', mapbox_access_token=mapbox_access_token)
+@app.route('/recommendations', methods=['GET','POST'])
+def recommendation():
+	return render_template('recommendations.html')
+
+@app.route('/urgent', methods=['GET','POST'])
+def urgent():
+	return render_template('urgent.html')
+
+@app.route('/admin', methods=['GET','POST'])
+def admin():
+	return render_template('admin.html')
