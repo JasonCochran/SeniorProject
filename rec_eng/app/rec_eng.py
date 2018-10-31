a627a28bdbdd9450d26b1f7ffdc1160d647d86b0
Jason Cochran <j.a.cochran.cs@gmail.com>
2018-10-31 14:14:02 -0400

    Sync db models and adding rec_eng.py changes

--- rec_eng/app/rec_eng.py
+++ rec_eng/app/rec_eng.py
@@ -4,5 +4,13 @@ from app import app, db, models
 import os, sys, requests
 
 def createRecommendation():
-
+	profiles = db.session.query(models.RecommendationProfile),all()
+	for profile in profiles:
+		# Loop through each profile and look for things that fit this profile...
+		# We pull the dataFeeder attribute and look for a table (maybe have this data stored as files?)
+		# Maybe also require that the file has a specific format or has certain headers?
+		# We can then run stats for each box over chicago against our imported data
+		# This will consist of checking how far each box is from the other data we pulled in
+		# Maybe we should do some sort of 'density' statistics to check for how many
+		#		attributes exist in a certain area
 	return "success"
